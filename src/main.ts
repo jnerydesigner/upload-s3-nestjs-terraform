cf41521c1ee1e3abcd1b10e9724ee6702dbcb026
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-07-10 11:01:00 -0400

    feat: atualizar configuração do servidor HTTPS para usar variáveis de ambiente e adicionar validação de certificados

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,45 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
-import * as fs from 'fs';
 import * as https from 'https';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const httpsOptions = {
-    key: fs.readFileSync('./certs/key.pem'),
-    cert: fs.readFileSync('./certs/cert.pem'),
-  };
+  const config = app.get(ConfigService);
+  app.enableCors({
+    origin: '*',
+  });
 
-  const config = new ConfigService();
-  const port = config.get<number>('SERVER_PORT') || 3000;
   const logger = new Logger('Bootstrap');
 
-  // Start HTTPS server
+  // Recupera e ajusta as variáveis de ambiente
+  const sslKey = config.get<string>('SSL_KEY');
+  const sslCert = config.get<string>('SSL_CERT');
+
+  if (!sslKey || !sslCert) {
+    logger.error('SSL_KEY or SSL_CERT is missing. HTTPS server cannot start.');
+    process.exit(1);
+  }
+
+  const httpsOptions = {
+    key: sslKey.replace(/\\n/g, '\n'),
+    cert: sslCert.replace(/\\n/g, '\n'),
+  };
+
+  const port = config.get<number>('SERVER_PORT') || 3000;
+
   const server = https.createServer(
     httpsOptions,
     app.getHttpAdapter().getInstance(),
   );
+
   await app.init();
+
   server.listen(port, () => {
-    logger.log(`Application is running on: http://localhost:${port}`);
+    logger.log(`Application is running on: https://localhost:${port}`);
   });
 }
+
 bootstrap();
