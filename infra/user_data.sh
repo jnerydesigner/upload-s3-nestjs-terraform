a3fd5f3bbfc3846de4bf07c4e590f7726995e665
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-07-11 10:13:03 -0400

    feat: reativar etapa de destruição do Terraform no workflow de destruição da infraestrutura fix: corrigir execução da etapa de destruição do Terraform no workflow de deploy
    feat: usar variáveis de ambiente para credenciais do AWS no script user_data.sh

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -9,3 +9,13 @@ systemctl enable docker
 systemctl start docker
 
 usermod -aG docker ec2-user
+
+
+
+docker run -d --restart unless-stopped -p 3388:3388 \
+  -v /home/ec2-user/certs:/app/certs \
+  -e AWS_BUCKET_NAME=s3-nest-upload \
+  -e AWS_REGION=us-east-1 \
+  -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+  -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+  jandernery/upload-s3-nodejs:latest
